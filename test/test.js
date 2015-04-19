cc90d86d2f88007f939c210a1a3a44c05acaebf2
Nolan Lawson <nolan.lawson@gmail.com>
2015-04-19 15:08:20 -0400

    fix several bugs

--- test/test.js
+++ test/test.js
@@ -3,16 +3,7 @@
 
 var SocketPouch = require('../lib/client');
 
-/*function WrappedSocketPouch(opts, callback) {
-  var url = 'ws://localhost:8080';
-  var name = opts.originalName;
-  return new SocketPouch({url: url, name: name}, callback);
-}
-
-WrappedSocketPouch.valid = SocketPouch.valid;
-WrappedSocketPouch.destroy = SocketPouch.destroy;
-
-window.PouchDB.adapter('socket', WrappedSocketPouch);*/
+window.PouchDB = require('pouchdb');
 
 window.PouchDB.adapter('socket', SocketPouch);
 window.PouchDB.preferredAdapters = ['socket'];
