06c68402ef7136b52bdf9bd5e5e40486a05ebccd
Nolan Lawson <nolan.lawson@gmail.com>
2015-10-05 22:21:16 -0400

    run travis in 4 tests

--- bin/test-browser.js
+++ bin/test-browser.js
@@ -28,6 +28,10 @@ if (process.env.SUITE === '1') {
   baseUrl = 'http://127.0.0.1:8000/test/index-suite1.html';
 } else if (process.env.SUITE === '2') {
   baseUrl = 'http://127.0.0.1:8000/test/index-suite2.html';
+} else if (process.env.SUITE === '3') {
+  baseUrl = 'http://127.0.0.1:8000/test/index-suite3.html';
+} else if (process.env.SUITE === '4') {
+  baseUrl = 'http://127.0.0.1:8000/test/index-suite4.html';
 }
 var testUrl = baseUrl + '?grep=3955%7Cclosure&invert=true';
 var qs = {};
