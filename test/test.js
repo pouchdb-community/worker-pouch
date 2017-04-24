bed4678ade03faf80add6236b2c320716a1d6e0b
John Kleinschmidt <kleinschmidtorama@gmail.com>
2017-04-24 16:22:40 -0400

    Pull PouchDB from utils instead of from pouchdb-browser

--- test/test.js
+++ test/test.js
@@ -3,8 +3,6 @@
 
 var WorkerPouch = require('../lib/client');
 
-window.PouchDB = require('pouchdb-browser');
-
 window.PouchDB.adapter('worker', WorkerPouch);
 window.PouchDB.preferredAdapters = ['worker'];
 
