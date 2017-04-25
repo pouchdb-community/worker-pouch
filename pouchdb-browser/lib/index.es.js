e25133bb6873c880ccddd9e4d285373f723f73f9
John Kleinschmidt <kleinschmidtorama@gmail.com>
2017-04-25 12:01:27 -0400

    Throw Error, not exception

--- pouchdb-browser/lib/index.es.js
+++ pouchdb-browser/lib/index.es.js
@@ -8626,7 +8626,7 @@ function readAttachmentsAsBlobOrBuffer(row) {
   Object.keys(atts).forEach(function (filename) {
     var att = atts[filename];
     if (!att.data) {
-      throw new Exception('IN readAttachmentsAsBlobOrBuffer, att data undefined, doc was: ', JSON.stringify(row.doc, null, 2));
+      throw new Error('IN readAttachmentsAsBlobOrBuffer, att data undefined, doc was: ', JSON.stringify(row.doc, null, 2));
     }
     att.data = b64ToBluffer(att.data, att.content_type);
   });
@@ -9991,7 +9991,7 @@ function createAbstractMapReduce(localDocName, mapper, reducer, ddocValidator) {
       Object.keys(atts).forEach(function (filename) {
         var att = atts[filename];
         if (!att.data) {
-          throw new Exception('IN readAttachmentsAsBlobOrBuffer2, att data undefined, doc was: ', JSON.stringify(row.doc, null, 2));
+          throw new Error('IN readAttachmentsAsBlobOrBuffer2, att data undefined, doc was: ', JSON.stringify(row.doc, null, 2));
         }
         atts[filename].data = b64ToBluffer(att.data, att.content_type);
       });
